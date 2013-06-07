bec865eb2cfe6a8c87625733238fb3370952cff3
maisaengineering <contact@maisasolutions.com>
2013-06-07 14:38:13 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogging::Application.config.secret_token = '40dbbdbceb3a3446802da38d9c499b8bc5120138982c453d94fb33f9103ec59da445daad7f2023fa81fa31a584dd902c8e0e17e308f8f8bdf642cdafd9468670'
